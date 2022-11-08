608626703c48272512881db4ff526f0963a83252
ahm-rubayed <rubayedahmed04@gmail.com>
2022-11-08 15:06:06 +0600

    services data added

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+require('dotenv').config()
 
 
 const app = express()
@@ -7,6 +9,34 @@ const port = process.env.PORT || "5000";
 
 app.use(cors())
 app.use(express.json())
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster1.v8hhiog.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+        const serviceCollection = client.db('proShoot').collection('services')
+
+        app.get("/services", async(req, res) => {
+            const query = {}    
+            const cursor = serviceCollection.find(query)
+            const services = await cursor.toArray()
+            res.send(services)
+        })
+
+        app.get("/services/:id", async(req, res) => {
+            const id = req.params.id;
+            const query = {_id: ObjectId(id)}
+            const service = await serviceCollection.findOne(query)
+            res.send(service)
+        })
+    }
+    finally{
+
+    }
+}
+run().catch(error => console.error(error))
 
 app.get("/", (req, res) => {
     res.send("ProShoot server is running")
