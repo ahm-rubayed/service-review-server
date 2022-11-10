eb3c3907b64f3c334b02bf25c936afb65cceb0ea
ahm-rubayed <rubayedahmed04@gmail.com>
2022-11-10 11:32:20 +0600

    jwt added

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const cors = require('cors');
+const jwt = require('jsonwebtoken')
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 
 
-const app = express()
-const port = process.env.PORT || "5000";
+const app = express();
+const port = process.env.PORT || 5000;
 
 app.use(cors())
 app.use(express.json())
@@ -14,9 +15,34 @@ app.use(express.json())
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster1.v8hhiog.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
+function verifyJWT(req, res, next) {
+    const authHeader = req.headers.authorization;
+
+    if(!authHeader) {
+        return res.status(401).send({message: 'unauthorized access'});
+    }
+    const token = authHeader.split(' ')[1];
+    
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function(err, decoded){
+        if(err){
+            return res.status(403).send({message: 'Forbidden access'});
+        }
+        req.decoded = decoded;
+        next();
+    })
+}
+
 async function run() {
     try {
-        const serviceCollection = client.db('proShoot').collection('services')
+        const serviceCollection = client.db('proShooter').collection('services')
+        const reviewCollection = client.db('proShooter').collection('reviews')
+        const customServiceCollection = client.db('proShooter').collection('customService')
+
+        app.post('/jwt', (req, res) =>{
+            const user = req.body;
+            const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1d'})
+            res.send({token})
+        }) 
 
         app.get("/services", async(req, res) => {
             const query = {}    
@@ -30,6 +56,45 @@ async function run() {
             const query = {_id: ObjectId(id)}
             const service = await serviceCollection.findOne(query)
             res.send(service)
+        })
+
+        app.get('/reviews', async (req, res) => {
+            let query = {};
+            if (req.query.email) {
+                query = {
+                    email: req.query.email
+                }
+            }
+            const cursor = reviewCollection.find(query);
+            const reviews = await cursor.toArray();
+            res.send(reviews);
+        });
+
+
+        app.get('/addservice', async (req, res) => {
+            const query = {}
+            const cursor = customServiceCollection.find(query);
+            const service = await cursor.toArray();
+            res.send(service);
+        });
+
+        app.post("/reviews", async(req, res) => {
+            const review = req.body;
+            const result = await reviewCollection.insertOne(review)
+            res.send(result)
+        })
+
+        app.post("/addservice", async(req, res) => {
+            const service = req.body;
+            const result = await customServiceCollection.insertOne(service)
+            res.send(result)
+        })
+
+        app.delete('/reviews/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await reviewCollection.deleteOne(query);
+            res.send(result);
         })
     }
     finally{
